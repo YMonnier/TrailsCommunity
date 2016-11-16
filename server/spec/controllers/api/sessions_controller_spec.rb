9655cc88cae6f9493f38fdf52de549c5ac286ea1
Ysee Monnier <yseemonnier@gmail.com>
2016-11-16 13:55:54 +0100

    improve test(sessions#index)

--- server/spec/controllers/api/sessions_controller_spec.rb
+++ server/spec/controllers/api/sessions_controller_spec.rb
@@ -60,4 +60,52 @@ RSpec.describe Api::SessionsController, :type => :controller do
             it { should respond_with 400 }
         end
     end
+
+    describe 'GET #index' do
+        context 'successfully' do
+            before do
+                @user = FactoryGirl.create :user
+                @sessions = [FactoryGirl.create(:session), FactoryGirl.create(:session)]
+
+                # Add Authorization
+                token = generate_token @user
+                api_authorization_header token
+
+                get :index
+
+                @json = json_response
+                @data = @json[:data]
+            end
+
+            it 'should be the same size array' do
+                expect(@data.size).to eql @sessions.size
+            end
+
+            it 'should be the same activity' do
+                expect(@data[0][:activity]).to eql @sessions[0].activity
+            end
+
+            it 'should be the same departure place' do
+                expect(@data[0][:departure_place]).to eql @sessions[0].departure_place
+            end
+
+            it 'should be the same arrival place' do
+                expect(@data[0][:arrival_place]).to eql @sessions[0].arrival_place
+            end
+
+            it 'should be the same activity' do
+                expect(@data[1][:activity]).to eql @sessions[1].activity
+            end
+
+            it 'should be the same departure place' do
+                expect(@data[1][:departure_place]).to eql @sessions[1].departure_place
+            end
+
+            it 'should be the same arrival place' do
+                expect(@data[1][:arrival_place]).to eql @sessions[1].arrival_place
+            end
+
+            it { should respond_with 200 }
+        end
+    end
 end
