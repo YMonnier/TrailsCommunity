8290064822278dba39891de6e96c8ef8a97afb23
Ysee Monnier <yseemonnier@gmail.com>
2016-11-19 14:00:47 +0100

    delete old file

---
+++ app/android/TrailsCommunity/app/src/main/java/fr/univ_tln/trailscommunity/features/sessions/SessionsActivity.java
@@ -0,0 +1,14 @@
+package fr.univ_tln.trailscommunity.features.sessions;
+
+/**
+ * Project TrailsCommunity.
+ * Package fr.univ_tln.trailscommunity.features.session.
+ * File SessionsActivity.java.
+ * Created by Ysee on 19/11/2016 - 13:48.
+ * www.yseemonnier.com
+ * https://github.com/YMonnier
+ */
+
+
+public class SessionsActivity {
+}
