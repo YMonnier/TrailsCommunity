9b3f14e6ba6273553749fd0b0b6f14f3def9826e
Stephen <stephenbellanger@gmail.com>
2016-11-23 17:26:23 +0100

    Add chat structure

--- app/android/TrailsCommunity/app/src/main/java/fr/univ_tln/trailscommunity/features/session/SessionActivity.java
+++ app/android/TrailsCommunity/app/src/main/java/fr/univ_tln/trailscommunity/features/session/SessionActivity.java
@@ -1,7 +1,15 @@
 package fr.univ_tln.trailscommunity.features.session;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,18 +21,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import fr.univ_tln.trailscommunity.R;
 
 
-public class SessionActivity extends FragmentActivity implements OnMapReadyCallback {
+public class SessionActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private String[] mChatMessages;
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout mDrawerLinear;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.session_session_activity);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mChatMessages = getResources().getStringArray(R.array.activities);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLinear = (LinearLayout) findViewById(R.id.left_drawer);
+        mDrawerList = (ListView) findViewById(R.id.my_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mChatMessages));
     }
 
 
@@ -46,4 +69,24 @@ public class SessionActivity extends FragmentActivity implements OnMapReadyCallb
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.session_session_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.session_chat_menu:
+                mDrawerLayout.openDrawer(mDrawerLinear);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
