da4ebfe811c0cb45ae873bba31e9f030f6148322
Ysee Monnier <yseemonnier@gmail.com>
2016-11-28 22:17:23 +0100

    update location service add onProviderEnabled action

--- app/android/TrailsCommunity/app/src/main/java/fr/univ_tln/trailscommunity/features/session/navigation/location/LocationService.java
+++ app/android/TrailsCommunity/app/src/main/java/fr/univ_tln/trailscommunity/features/session/navigation/location/LocationService.java
@@ -1,8 +1,10 @@
 package fr.univ_tln.trailscommunity.features.session.navigation.location;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -49,18 +52,35 @@ public class LocationService extends Service implements LocationListener {
         super.onStart(intent, startId);
         Log.d(TAG, "onStart");
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 4000, 0, listener);
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 4000, 0, listener);
+
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d(TAG, "ActivityCompat.checkSelfPermission::OK");
+
+            return;
+        } else {
+            Log.d(TAG, "ActivityCompat.checkSelfPermission::NOT OK");
+        }
     }
+
 
     @Override
     public void onLocationChanged(Location location) {
-
+        Log.d(TAG, "location updated: " + location);
     }
 
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
-
+        Log.d(TAG, "onStatusChanged: " + s + " | " + i);
     }
 
     @Override
@@ -72,6 +92,7 @@ public class LocationService extends Service implements LocationListener {
     @Override
     public void onProviderDisabled(String s) {
         Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         Toast.makeText(getBaseContext(), "Gps is turned off!! ",
                 Toast.LENGTH_SHORT).show();
